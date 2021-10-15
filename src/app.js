a71ca77510bb96388f94c29da62bd1056fe23904
kulakoff <anton.kulakoff@gmail.com>
2021-10-16 01:10:47 +0300

    add auth.js and smal changes in app.js

--- src/app.js
+++ src/app.js
@@ -1,28 +1,53 @@
-import { isValid } from './utils'
+import { Question } from './question';
+import { createModal, isValid } from './utils'
+import { getAuthFtom } from './auth';
 import './styles.css'
 
 
 
+
 const form = document.getElementById('form');
+const modalBtn = document.getElementById('modal-btn');
 const input = form.querySelector('#question-input');
 const submitBtn = form.querySelector('#submit');
 
-
+window.addEventListener('load', Question.renderList)
 form.addEventListener('submit', submitFormHandler);
+modalBtn.addEventListener('click', openModal)
+input.addEventListener('input', () => {
+    submitBtn.disabled = !isValid(input.value);
+})
 
 function submitFormHandler(event) {
     event.preventDefault()
     if (isValid(input.value)) {
         const question = {
-            test: input.value.trim(),
+            text: input.value.trim(),
             date: new Date().toJSON()
         };
         submitBtn.disbled = true;
         //async request to server, save question
+        Question.create(question).then(() => {
+            input.value = '';
+            input.className = '';
+            submitBtn.disabled = false;
+        })
         console.log('Question : ', question);
-        input.value = '';
-        input.className = '';
-        submitBtn.disabled = false;
     }
+}
 
+
+
+function openModal() {
+    createModal('Авторизация', getAuthFtom());
+    document
+        .getElementById('auth-form')
+        .addEventListener('submit', authFormHandler, { once: true });
+}
+
+function authFormHandler(event) {
+    event.preventDefault();
+    const email = event.target.querySelector('#email').value;
+    const password = event.target.querySelector('#password').value;
+    console.log(email, password)
 }
